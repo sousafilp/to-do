336f2667ef61adec7e6a3906551aab5698fb585b
sousafilp <sousaafilp@gmail.com>
2024-04-27 15:59:10 -0300

    conectando controle, rotas e view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes/routes");
 
 const app = express();
 const port = 3000;
@@ -7,10 +8,8 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// ROTA
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// ROTAS
+app.use(routes);
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
