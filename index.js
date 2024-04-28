fcb50dfb0460821c7fd37b33cdd651fe164f161d
sousafilp <sousaafilp@gmail.com>
2024-04-28 00:11:09 -0300

    Foram implementadas as funcionalidades de criar e ler tarefas

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const path = require("path");
 const routes = require("./routes/routes");
 const connectToDb = require("./database/db");
 
+connectToDb();
 const app = express();
 const port = 3000;
 
-connectToDb();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
-// ROTAS
+app.use(express.urlencoded());
 app.use(routes);
 
 app.listen(port, () =>
