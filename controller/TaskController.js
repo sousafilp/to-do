fe1a216b2c0c849b881dbdcf73ddddb94ba6009d
sousafilp <sousaafilp@gmail.com>
2024-04-29 09:01:16 -0300

    Adição da funcionalidade encontrar por id e editar tarefa

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", { tasksList });
+    return res.render("index", { tasksList, task: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,29 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    res.render("index", { task, tasksList });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
 };
