8fa00eed80c067a967b418f661681901aca9dcff
sousafilp <sousaafilp@gmail.com>
2024-04-30 09:36:59 -0300

    Adicionando a função de delete e o modal para confirmação

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", { tasksList, task: null });
+    return res.render("index", { tasksList, task: null, taskDelete: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -26,9 +26,14 @@ const createTask = async (req, res) => {
 
 const getById = async (req, res) => {
   try {
-    const task = await Task.findOne({ _id: req.params.id });
     const tasksList = await Task.find();
-    res.render("index", { task, tasksList });
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, tasksList });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, tasksList });
+    }
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -44,9 +49,21 @@ const updateOneTask = async (req, res) => {
   }
 };
 
+const deleteOneTask = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
   getById,
   updateOneTask,
+  deleteOneTask,
 };
